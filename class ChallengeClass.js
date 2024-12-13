aeef7e288c1a9496a8b87d19fbe48995ba82ee8d
Vi Thi Lan <lanvt6596@ut.edu.vn>
2024-12-13 14:01:08 +0700

    hello

--- class ChallengeClass.js
+++ class ChallengeClass.js
@@ -15,3 +15,4 @@ console.log(challenge.IsValid("10/2+3)")); // Output: 0
 console.log(challenge.IsValid("1++2")); // Output: 0
 console.log(challenge.IsValid("5/(3-3)")); // Output: 0
 console.log(challenge.IsValid("4-")); // Output: 0
+console.log("hello world");
